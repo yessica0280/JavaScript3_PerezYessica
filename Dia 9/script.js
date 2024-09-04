65e9ac11325987d8f9b634fb234f68950ceef11d
yessica0280 <yp80905@gmail.com>
2024-09-04 12:25:06 -0500

    feat :sparkles: Initial commit

---
+++ Dia 9/script.js
@@ -0,0 +1,54 @@
+const pokemonName =document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon__image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+const buttonPrev = document.querySelector('.btn_prev');
+const buttonNext =document.querySelector('.btn_next');
+
+let searchPokemon = 1;
+
+const fetchPokemon = async(pokemon) =>{
+    const APIResponse =await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    if (APIResponse.status==200){
+        const data= await APIResponse.json();
+        return data;
+    }
+}
+
+const renderPokemon = async(pokemon) =>{
+    pokemonName.innerHTML ='loading...';
+    pokemonNumber.innerHTML='';
+
+    const data = await fetchPokemon(pokemon);
+
+    if (data){
+        pokemonImage.style.display='block';
+        pokemonName.innerHTML=data.name;
+        pokemonNumber.innerHTML=data.id;
+        pokemonImage.src=data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value='';
+        searchPokemon=data.id;
+    }else{
+        pokemonImage.stye.display='none';
+        pokemonName.innerHTML="No se encontro :sad feis:";
+        pokemonNumber.innerHTML='';
+    }
+}
+
+form.addEventListener('submit', (even)=>{
+    even.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+})
+buttonPrev.addEventListener('click', ()=>{
+    if (searchPokemon > 1){
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }
+});
+
+buttonNext.addEventListener('click', ()=>{
+        searchPokemon += 1;
+        renderPokemon(searchPokemon);
+});
